ea45b9d9a496e4e151b503c1298173af933047f0
Abhay Kumar <abhayprofesswork@gmail.com>
2025-07-09 17:09:51 +0530

    Update Index.js
    Add Comments

--- Index.js
+++ Index.js
@@ -5,15 +5,15 @@ const server = http.createServer((req, res) => {
     if (req.url === '/') {  //req.url always begins with /.
         res.write("This is the Home page");
         res.end();
-    } else if (req.url === '/login' && req.method === 'GET') {
+    } else if (req.url === '/login' && req.method === 'GET') {  //This when we have route '/login'
         fs.readFile(__dirname + '/login.html', 'utf-8', (err, data) => {
             if (!err) {
                 res.write(data);
                 res.end();
             }
         });
-    }else if(req.url=='/login' && req.method==='POST'){
-        let body=''
+    }else if(req.url=='/login' && req.method==='POST'){   //This when we have route /login and we fill login details and then POST
+        let body=''                                        // Request serve
         req.on('data',(chunk)=>{
             body+=chunk.toString()
         })
